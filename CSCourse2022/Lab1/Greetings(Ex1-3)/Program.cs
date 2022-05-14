73792d16822c679aee44a34e885ff05de120bdc6
Ilia <96974402+Ilia-Ivanov-2022@users.noreply.github.com>
2022-05-14 17:43:01 +0300

    Add files via upload

---
+++ CSCourse2022/Lab1/Greetings(Ex1-3)/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITMO.CSCourse2022.Greetings
+{
+    internal class Greeter
+    {
+        static void Main(string[] args)
+        {
+            string myName;
+
+            Console.WriteLine("What is your name please?");
+            myName = Console.ReadLine();
+            Console.WriteLine("Hello {0}", myName);
+        }
+    }
+}
