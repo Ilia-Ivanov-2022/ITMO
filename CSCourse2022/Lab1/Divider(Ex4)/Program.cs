3c5216371180150cc42b0ab7a6cb5d3db9ff85e0
Ilia <96974402+Ilia-Ivanov-2022@users.noreply.github.com>
2022-05-14 17:38:16 +0300

    Lab #1, Exercise #4 - Divider
    Catching divide by 0 error.

---
+++ CSCourse2022/Lab1/Divider(Ex4)/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITMO.CSCourse2022.Divider
+{
+    internal class DivideIt
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter two integers.\nPlease enter the first one:");
+                string temp = Console.ReadLine();
+                int i = Int32.Parse(temp);
+
+                Console.WriteLine("Please enter the second integer:");
+                temp = Console.ReadLine();
+                int j = Int32.Parse(temp);
+
+                int k = i / j;
+                Console.WriteLine("Result of dividing is {0}", k);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An exception was thrown: {0}", e);
+            }
+        }
+        
+    }
+}
