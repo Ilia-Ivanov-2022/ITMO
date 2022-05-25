478e4e0d158bb8ee8c280efb6572da9931a35f2e
Ilia <96974402+Ilia-Ivanov-2022@users.noreply.github.com>
2022-05-25 15:52:43 +0300

    Add files via upload

---
+++ CSCourse2022/Lab3/Exercise1.WhatDay1/Version2/WhatDay.cs
@@ -0,0 +1,193 @@
+﻿using System;
+
+namespace WhatDay1
+{
+    enum MonthName
+    {
+        January,
+        February,
+        March,
+        April,
+        May,
+        June,
+        July,
+        August,
+        September,
+        October,
+        November,
+        December
+    }
+
+    class WhatDay
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a day number between 1 and 365:");
+            string line = Console.ReadLine();
+            int dayNum = int.Parse(line);
+
+            int monthNum = 0;
+
+            ////////////////////////////////////////////////////////////////////
+            ////// Uncomment the code below  before Calculating           //////
+            /////  the month and day pair from a day number               //////
+            ////////////////////////////////////////////////////////////////////
+
+            if (dayNum <= 31)
+            { // January
+                goto End;
+            }
+            else
+            {
+                dayNum -= 31;
+                monthNum++;
+            }
+
+            if (dayNum <= 28)
+            { // February
+                goto End;
+            }
+            else
+            {
+                dayNum -= 28;
+                monthNum++;
+            }
+
+            if (dayNum <= 31)
+            { // March
+                goto End;
+            }
+            else
+            {
+                dayNum -= 31;
+                monthNum++;
+            }
+
+            if (dayNum <= 30)
+            { // April
+                goto End;
+            }
+            else
+            {
+                dayNum -= 30;
+                monthNum++;
+            }
+
+            if (dayNum <= 31)
+            { // May
+                goto End;
+            }
+            else
+            {
+                dayNum -= 31;
+                monthNum++;
+            }
+
+
+            if (dayNum <= 30)
+            { // June
+                goto End;
+            }
+            else
+            {
+                dayNum -= 30;
+                monthNum++;
+            }
+
+            if (dayNum <= 31)
+            { // July
+                goto End;
+            }
+            else
+            {
+                dayNum -= 31;
+                monthNum++;
+            }
+
+            if (dayNum <= 31)
+            { // August
+                goto End;
+            }
+            else
+            {
+                dayNum -= 31;
+                monthNum++;
+            }
+
+            if (dayNum <= 30)
+            { // September
+                goto End;
+            }
+            else
+            {
+                dayNum -= 30;
+                monthNum++;
+            }
+
+            if (dayNum <= 31)
+            { // October
+                goto End;
+            }
+            else
+            {
+                dayNum -= 31;
+                monthNum++;
+            }
+
+            if (dayNum <= 30)
+            { // November
+                goto End;
+            }
+            else
+            {
+                dayNum -= 30;
+                monthNum++;
+            }
+
+            if (dayNum <= 31)
+            { // December
+                goto End;
+            }
+            else
+            {
+                dayNum -= 31;
+                monthNum++;
+            }
+
+        End:
+
+            MonthName temp = (MonthName)monthNum;
+            string monthName = temp.ToString();
+
+
+            //////////////////////////////////////////////////////////////
+            // TODO: Implement if statements for November and December ///
+            //////////////////////////////////////////////////////////////
+
+
+            //////////////////////////////////////////////////////////////
+            // TODO:												  ////
+            //		Add an identifer label End below and              ////
+            //		declare string variable							  ////
+            //////////////////////////////////////////////////////////////
+
+            
+
+            //////////////////////////////////////////////////////////////////
+            // TODO: Implement the case 10 and case 11 for November         //
+            //       and December											//
+            //       Add a default label									//
+            //////////////////////////////////////////////////////////////////
+
+
+            Console.WriteLine("The entered day is {0}th of {1}", dayNum, monthName);
+        }
+
+            
+
+            // Don't modify anything below here
+            static System.Collections.ICollection DaysInMonths
+            = new int[12] { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+    }
+}
