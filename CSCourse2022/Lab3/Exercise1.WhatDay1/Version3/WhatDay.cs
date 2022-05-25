1b77caa7a5fb820bdad368ae4437b0672a75a267
Ilia <96974402+Ilia-Ivanov-2022@users.noreply.github.com>
2022-05-25 16:14:37 +0300

    Add files via upload

---
+++ CSCourse2022/Lab3/Exercise1.WhatDay1/Version3/WhatDay.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace WhatDay1
+{
+    enum MonthName
+    {
+        January,
+        February,
+        March,
+        April,
+        May,
+        June,
+        July,
+        August,
+        September,
+        October,
+        November,
+        December
+    }
+
+    class WhatDay
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a day number between 1 and 365:");
+            string line = Console.ReadLine();
+            int dayNum = int.Parse(line);
+
+            int monthNum = 0;
+
+            foreach(int daysInMonth in DaysInMonths)
+            {
+                if (dayNum <= daysInMonth) break;
+                else
+                {
+                    dayNum -= daysInMonth;
+                    monthNum++;
+                }
+            }
+
+        
+            MonthName temp = (MonthName)monthNum;
+            string monthName = temp.ToString();
+
+           
+            Console.WriteLine("The entered day is {0}th of {1}", dayNum, monthName);
+        }
+
+            
+
+            // Don't modify anything below here
+            static System.Collections.ICollection DaysInMonths
+            = new int[12] { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+    }
+}
