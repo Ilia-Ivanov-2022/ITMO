ed1329ea7cdb188b24d9262de4f4b3a6b57ddc82
Ilia <96974402+Ilia-Ivanov-2022@users.noreply.github.com>
2022-05-29 00:09:28 +0300

    Add files via upload

---
+++ CSCourse2022/Lab7/Exercise1.Bank/Test.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class Test
+{
+    public static void Main()
+    {
+        BankAccount ba1 = new BankAccount();
+        ba1.Populate(100);
+                
+        BankAccount ba2 = new BankAccount ();
+        ba2.Populate(100);
+
+        Console.WriteLine("Before Transfer:\nAccount type: {0}\tAccount number: {1}\tAccount balance: {2}",
+                          ba1.Type(),
+                          ba1.Number(),
+                          ba1.Balance());
+        Console.WriteLine("Before Transfer:\nAccount Type: {0}\tAccount number: {1}\tAccount balance: {2}",
+                          ba2.Type(),
+                          ba2.Number(),
+                          ba2.Balance());
+
+        ba1.TransferFrom(ba2, 10);
+
+        Console.WriteLine("After Transfer:\nAccount type: {0}\tAccount number: {1}\tAccount balance: {2}",
+                          ba1.Type(),
+                          ba1.Number(),
+                          ba1.Balance());
+        Console.WriteLine("After Transfer:\nAccount Type: {0}\tAccount number: {1}\tAccount balance: {2}",
+                          ba2.Type(),
+                          ba2.Number(),
+                          ba2.Balance());
+    }
+}
