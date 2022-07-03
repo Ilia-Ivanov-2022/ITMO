48c1f6d60aa221489871623f6f51d64c9720700a
Ilia <96974402+Ilia-Ivanov-2022@users.noreply.github.com>
2022-07-03 23:20:50 +0300

    Add files via upload

---
+++ CSAppDev/ITMO.CSAppDev.Lab2.Exercise1.WinQuestion/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace ITMO.CSAppDev.Lab2.Exercise1.WinQuestion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnyes = new System.Windows.Forms.Button();
+            this.btnno = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnyes
+            // 
+            this.btnyes.Location = new System.Drawing.Point(21, 84);
+            this.btnyes.Name = "btnyes";
+            this.btnyes.Size = new System.Drawing.Size(131, 42);
+            this.btnyes.TabIndex = 0;
+            this.btnyes.Text = "Yes";
+            this.btnyes.UseVisualStyleBackColor = true;
+            this.btnyes.Click += new System.EventHandler(this.btnyes_Click);
+            // 
+            // btnno
+            // 
+            this.btnno.Location = new System.Drawing.Point(185, 84);
+            this.btnno.Name = "btnno";
+            this.btnno.Size = new System.Drawing.Size(131, 42);
+            this.btnno.TabIndex = 1;
+            this.btnno.Text = "No";
+            this.btnno.UseVisualStyleBackColor = true;
+            this.btnno.MouseMove += new System.Windows.Forms.MouseEventHandler(this.btnno_MouseMove);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(73, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(209, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Are You satisfied with your salary?";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(328, 149);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnno);
+            this.Controls.Add(this.btnyes);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "Hot topic";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnyes;
+        private System.Windows.Forms.Button btnno;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
