665fb2f50c0bd901039d92867deeddf0816debd3
Ilia <96974402+Ilia-Ivanov-2022@users.noreply.github.com>
2022-07-04 12:13:47 +0300

    Add files via upload

---
+++ CSAppDev/ITMO.CSAppDev.Lab1.Exercise4/ChildForm.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ITMO.CSAppDev.Lab1.Exercise4
+{
+    public partial class ChildForm : Form
+    {
+        public ChildForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ToggleMenuItem_Click(object sender, EventArgs e)
+        {
+            if (ToggleMenuItem.Checked)
+            {
+                ToggleMenuItem.Checked = false;
+                ChildTextBox.ForeColor = System.Drawing.Color.Black;
+            }
+            else
+            {
+                ToggleMenuItem.Checked = true;
+                ChildTextBox.ForeColor= System.Drawing.Color.Blue;
+            }
+        }
+    }
+}
