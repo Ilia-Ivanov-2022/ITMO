28b668a4834144f348284e98e83a7fe962f538d7
Ilia <96974402+Ilia-Ivanov-2022@users.noreply.github.com>
2022-07-11 11:24:33 +0300

    Add files via upload

---
+++ CSAppDev/Lab3/ITMO.CSAppDev.Lab3.Eercise2.WinTimer2/UserControlTimer2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ITMO.CSAppDev.Lab3.Exercise2.WinTimer2
+{
+    public partial class UserControlTimer2 : Control
+    {
+        public UserControlTimer2()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnPaint(PaintEventArgs pe)
+        {
+            base.OnPaint(pe);
+            // determine our rectangule
+            Graphics g = pe.Graphics;
+            g.FillRectangle(Brushes.Blue, 0, 0, this.Width, this.Height);
+            // determine timer
+            pe.Graphics.DrawString(DateTime.Now.ToLongTimeString(), this.Font,
+                new SolidBrush(this.ForeColor), 0, 0);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.Refresh();
+        }
+    }
+}
