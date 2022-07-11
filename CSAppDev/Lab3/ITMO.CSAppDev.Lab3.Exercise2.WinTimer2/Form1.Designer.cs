2e3717bd2c35640d94d42ce257aeeccfb5e7e301
Ilia <96974402+Ilia-Ivanov-2022@users.noreply.github.com>
2022-07-11 11:43:11 +0300

    Add files via upload

---
+++ CSAppDev/Lab3/ITMO.CSAppDev.Lab3.Exercise2.WinTimer2/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace ITMO.CSAppDev.Lab3.Exercise2.WinTimer2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.userControlTimer21 = new ITMO.CSAppDev.Lab3.Exercise2.WinTimer2.UserControlTimer2();
+            this.SuspendLayout();
+            // 
+            // userControlTimer21
+            // 
+            this.userControlTimer21.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.userControlTimer21.Location = new System.Drawing.Point(36, 27);
+            this.userControlTimer21.Name = "userControlTimer21";
+            this.userControlTimer21.Size = new System.Drawing.Size(266, 67);
+            this.userControlTimer21.TabIndex = 0;
+            this.userControlTimer21.Text = "userControlTimer21";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(341, 202);
+            this.Controls.Add(this.userControlTimer21);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private UserControlTimer2 userControlTimer21;
+    }
+}
+
