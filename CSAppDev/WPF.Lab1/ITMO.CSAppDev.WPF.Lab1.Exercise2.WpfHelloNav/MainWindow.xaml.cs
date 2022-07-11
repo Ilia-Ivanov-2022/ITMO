2c10b41817992d1d88a50218cf14380de3a33868
Ilia <96974402+Ilia-Ivanov-2022@users.noreply.github.com>
2022-07-11 21:26:28 +0300

    Add files via upload

---
+++ CSAppDev/WPF.Lab1/ITMO.CSAppDev.WPF.Lab1.Exercise2.WpfHelloNav/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Navigation;
+
+namespace ITMO.CSAppDev.WPF.Lab1.Exercise2.WpfHelloNav
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : NavigationWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ CSAppDev/WPF.Lab1/ITMO.CSAppDev.WPF.Lab1.Exercise2.WpfHelloNav/Page1.xaml.cs
@@ -0,0 +1,23 @@
+﻿
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ITMO.CSAppDev.WPF.Lab1.Exercise2.WpfHelloNav
+{
+    /// <summary>
+    /// Interaction logic for Page1.xaml
+    /// </summary>
+    public partial class Page1 : Page
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            Page2 page = new Page2();
+            this.NavigationService.Navigate(page);
+        }
+    }
+}
