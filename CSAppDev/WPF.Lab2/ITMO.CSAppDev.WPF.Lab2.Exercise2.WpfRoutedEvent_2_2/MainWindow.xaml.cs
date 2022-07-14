d4d4c245f994162e8366965d2b87ca00003d7c33
Ilia <96974402+Ilia-Ivanov-2022@users.noreply.github.com>
2022-07-14 22:09:06 +0300

    Add files via upload

---
+++ CSAppDev/WPF.Lab2/ITMO.CSAppDev.WPF.Lab2.Exercise2.WpfRoutedEvent_2_2/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ITMO.CSAppDev.WPF.Lab2.Exercise1.WpfRoutedEvent_2_2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void StackPanel_Click(object sender, RoutedEventArgs e)
+        {
+            FrameworkElement feSource = e.Source as FrameworkElement;
+            double a = Double.Parse(txtBox.Text);
+
+            switch (feSource.Name)
+            {
+                case "butAdd":
+                    a += a;
+                    break;
+                case "butMult":
+                    a *= a;
+                    break;
+            }
+            e.Handled = true;
+
+            txtBox.Text = String.Format("{0:#.##}", a);
+        }
+    }
+}
