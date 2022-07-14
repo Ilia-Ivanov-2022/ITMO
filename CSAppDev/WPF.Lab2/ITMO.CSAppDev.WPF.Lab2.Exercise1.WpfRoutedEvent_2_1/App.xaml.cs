4d7ee8701ae5d1a81d94ee91b1f328ab9637e80c
Ilia <96974402+Ilia-Ivanov-2022@users.noreply.github.com>
2022-07-14 21:00:22 +0300

    Add files via upload

---
+++ CSAppDev/WPF.Lab2/ITMO.CSAppDev.WPF.Lab2.Exercise1.WpfRoutedEvent_2_1/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ITMO.CSAppDev.WPF.Lab2.Exercise1.WpfRoutedEvent_2_1
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ CSAppDev/WPF.Lab2/ITMO.CSAppDev.WPF.Lab2.Exercise1.WpfRoutedEvent_2_1/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ITMO.CSAppDev.WPF.Lab2.Exercise1.WpfRoutedEvent_2_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            MessageBox.Show("Event by textBox");
+            e.Handled = (bool)radBut1.IsChecked;
+        }
+
+        private void Grid_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+            MessageBox.Show("Event by Grid");
+            e.Handled = (bool)radBut2.IsChecked;
+        }
+
+        private void Window_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+            MessageBox.Show("Event by Window");
+            e.Handled = (bool)radBut3.IsChecked;
+        }
+    }
+}
