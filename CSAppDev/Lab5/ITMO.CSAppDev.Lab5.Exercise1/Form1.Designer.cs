16d589257b7ff7254c3a407648573b0b2f28192e
Ilia <96974402+Ilia-Ivanov-2022@users.noreply.github.com>
2022-07-17 20:10:11 +0300

    Add files via upload

---
+++ CSAppDev/Lab5/ITMO.CSAppDev.Lab5.Exercise1/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace ITMO.CSAppDev.Lab5.Exercise1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ofd = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(43, 345);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(348, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(43, 373);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Open";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(43, 25);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(716, 299);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // ofd
+            // 
+            this.ofd.DefaultExt = "\"*.xls\", \"*.xlsx\"";
+            this.ofd.FileName = "openFileDialog1";
+            this.ofd.Filter = "\"Excel 2019(*.xlsx)|*.xlsx|Excel 2016(*.xlsx)|*.xlsx|Excel2003(*.xls)|*.xls\"";
+            this.ofd.Title = "Select the document you want to download";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.OpenFileDialog ofd;
+    }
+}
+
---
+++ CSAppDev/Lab5/ITMO.CSAppDev.Lab5.Exercise1/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using ExcelObj = Microsoft.Office.Interop.Excel;
+
+namespace ITMO.CSAppDev.Lab5.Exercise1
+{
+    public partial class Form1 : Form
+    {   // copy of Excel Application with all its atributes
+        ExcelObj.Application app = new ExcelObj.Application();
+        ExcelObj.Workbook workbook;
+        ExcelObj.Worksheet NwSheet;
+        ExcelObj.Range ShtRange;
+                
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // copy of data table
+        DataTable dt = new DataTable();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = ofd.FileName;
+
+                try
+                {
+                    // load existing worlbook. The only parameter is a file path.
+                    workbook = app.Workbooks.Open(ofd.FileName);
+                    // access to a worksheet number 1
+                    NwSheet = (ExcelObj.Worksheet)workbook.Sheets.get_Item(1);
+                    // cells containing values
+                    ShtRange = NwSheet.UsedRange;
+
+                    // load the first row from the table
+                    for (int Cnum = 1; Cnum <= ShtRange.Columns.Count; Cnum++)
+                    {
+                        dt.Columns.Add(new DataColumn((ShtRange.Cells[1, Cnum] as ExcelObj.Range).Value2.ToString()));
+                    }
+                    dt.AcceptChanges();
+
+                    // set a first row values as a column name
+                    string[] columnNames = new String[dt.Columns.Count];
+                    for (int i = 0; i < dt.Columns.Count; i++)
+                    {
+                        columnNames[0] = dt.Columns[i].ColumnName;
+                    }
+
+                    // load remaining rows and add to the table
+                    for (int Rnum = 2; Rnum <= dt.Rows.Count; Rnum++)
+                    {
+                        DataRow dr = dt.NewRow();
+                        for (int Cnum = 1; Cnum <= ShtRange.Columns.Count; Cnum++)
+                        {
+                            if ((ShtRange.Cells[Rnum, Cnum] as ExcelObj.Range).Value2 != null)
+                            {
+                                dr[Cnum - 1] = (ShtRange.Cells[Rnum, Cnum] as
+                                    ExcelObj.Range).Value2.ToString();
+                            }
+                        }
+                        dt.Rows.Add(dr);    // add loaded rows to the table
+                        dt.AcceptChanges();
+                    }
+                    // connect control to loaded table
+                    dataGridView1.DataSource = dt;
+                    app.Quit();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("You have not select a file to open", "Loading data...",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+    }
+}
+
