204b102766e4abfbeaf0b8f7cbd0bb5e035d2a67
Ilia <96974402+Ilia-Ivanov-2022@users.noreply.github.com>
2022-09-13 01:20:06 +0300

    Add files via upload

---
+++ ADO.NET/ITMO.ADO.NET.Lab2.Ex2.DBConn.ExecuteReader/FormMain.Designer.cs
@@ -0,0 +1,178 @@
+﻿using System.Data;
+
+namespace ITMO.ADO.NET.Lab1.Ex2.DBConnection
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.connectionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.connectionListToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.connectToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.disconnectToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblProductQuantity = new System.Windows.Forms.Label();
+            this.btnProductList = new System.Windows.Forms.Button();
+            this.lstProductList = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.connectionToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // connectionToolStripMenuItem
+            // 
+            this.connectionToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.connectionListToolStripMenuItem,
+            this.connectToolStripMenuItem,
+            this.disconnectToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.connectionToolStripMenuItem.Name = "connectionToolStripMenuItem";
+            this.connectionToolStripMenuItem.Size = new System.Drawing.Size(98, 24);
+            this.connectionToolStripMenuItem.Text = "Connection";
+            // 
+            // connectionListToolStripMenuItem
+            // 
+            this.connectionListToolStripMenuItem.Name = "connectionListToolStripMenuItem";
+            this.connectionListToolStripMenuItem.Size = new System.Drawing.Size(207, 26);
+            this.connectionListToolStripMenuItem.Text = "Connection Info";
+            this.connectionListToolStripMenuItem.Click += new System.EventHandler(this.clickConnList);
+            // 
+            // connectToolStripMenuItem
+            // 
+            this.connectToolStripMenuItem.Name = "connectToolStripMenuItem";
+            this.connectToolStripMenuItem.Size = new System.Drawing.Size(207, 26);
+            this.connectToolStripMenuItem.Text = "Open Connection";
+            this.connectToolStripMenuItem.Click += new System.EventHandler(this.clickConnect);
+            // 
+            // disconnectToolStripMenuItem
+            // 
+            this.disconnectToolStripMenuItem.Name = "disconnectToolStripMenuItem";
+            this.disconnectToolStripMenuItem.Size = new System.Drawing.Size(207, 26);
+            this.disconnectToolStripMenuItem.Text = "Close Connection";
+            this.disconnectToolStripMenuItem.Click += new System.EventHandler(this.clickDisconnect);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(207, 26);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(21, 47);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 28);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Product Quantity";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.clickBtnProductQuatity);
+            // 
+            // lblProductQuantity
+            // 
+            this.lblProductQuantity.AutoSize = true;
+            this.lblProductQuantity.Location = new System.Drawing.Point(170, 53);
+            this.lblProductQuantity.Name = "lblProductQuantity";
+            this.lblProductQuantity.Size = new System.Drawing.Size(104, 16);
+            this.lblProductQuantity.TabIndex = 2;
+            this.lblProductQuantity.Text = "Product Quantity";
+            // 
+            // btnProductList
+            // 
+            this.btnProductList.Location = new System.Drawing.Point(21, 102);
+            this.btnProductList.Name = "btnProductList";
+            this.btnProductList.Size = new System.Drawing.Size(127, 27);
+            this.btnProductList.TabIndex = 3;
+            this.btnProductList.Text = "Product List";
+            this.btnProductList.UseVisualStyleBackColor = true;
+            this.btnProductList.Click += new System.EventHandler(this.clickBtnProductList);
+            // 
+            // lstProductList
+            // 
+            this.lstProductList.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1});
+            this.lstProductList.HideSelection = false;
+            this.lstProductList.Location = new System.Drawing.Point(173, 102);
+            this.lstProductList.Name = "lstProductList";
+            this.lstProductList.Size = new System.Drawing.Size(180, 170);
+            this.lstProductList.TabIndex = 4;
+            this.lstProductList.UseCompatibleStateImageBehavior = false;
+            this.lstProductList.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Product Name";
+            this.columnHeader1.Width = 180;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lstProductList);
+            this.Controls.Add(this.btnProductList);
+            this.Controls.Add(this.lblProductQuantity);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "FormMain";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem connectionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem connectToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem disconnectToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem connectionListToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblProductQuantity;
+        private System.Windows.Forms.Button btnProductList;
+        private System.Windows.Forms.ListView lstProductList;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+    }
+}
+
---
+++ ADO.NET/ITMO.ADO.NET.Lab2.Ex2.DBConn.ExecuteReader/FormMain.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace ITMO.ADO.NET.Lab1.Ex2.DBConnection
+{
+    public partial class FormMain : Form
+    {
+        OleDbConnection connection = new OleDbConnection();
+       
+        // Form ctor.
+        public FormMain()
+        {
+            InitializeComponent();
+            // connection state.
+            this.connection.StateChange += new StateChangeEventHandler(connection_StateChange);
+        }
+        static string GetConnectionStringByName(string name)
+        {
+            string returnValue = null;
+            ConnectionStringSettings settings = 
+                ConfigurationManager.ConnectionStrings[name];
+            if(settings != null)
+            {
+                returnValue = settings.ConnectionString;
+            }
+            return returnValue;
+        }
+        string testConnect = GetConnectionStringByName("DBConnect.NorthwindConnectionString");
+
+        // Event handlers.
+        private void clickConnect(object sender, System.EventArgs e)
+        {
+            try
+            {
+                if(connection.State != ConnectionState.Open)
+                {
+                    connection.ConnectionString = testConnect;
+                    connection.Open();
+                    MessageBox.Show("Successfully connected to data base.");
+                }
+                else
+                    MessageBox.Show("Connection to data base already exists.");
+            }
+            catch (OleDbException XcpSQL)
+            {
+                foreach(OleDbError se in XcpSQL.Errors)
+                {
+                    MessageBox.Show(se.Message,
+                                    "SQL Error Code " + se.NativeError,
+                                    MessageBoxButtons.OK,
+                                    MessageBoxIcon.Information);
+                }
+            }
+            catch(Exception Xcp)
+            {
+                MessageBox.Show(Xcp.Message,
+                                "Unexpected Exception",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Information);
+            }
+        }
+
+        private void clickDisconnect(object sender, System.EventArgs e)
+        {
+            if(connection.State == ConnectionState.Open)
+            {
+                connection.Close();
+                MessageBox.Show("Connection to data base is closed.");
+            }
+            else
+                MessageBox.Show("Connection to data base already has been closed.");
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, System.EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void connection_StateChange(object sender, System.Data.StateChangeEventArgs e)
+        {
+            connectToolStripMenuItem.Enabled = (e.CurrentState == ConnectionState.Closed);
+            disconnectToolStripMenuItem.Enabled = (e.CurrentState == ConnectionState.Open);
+        }
+
+        private void clickConnList(object sender, EventArgs e)
+        {
+            ConnectionStringSettingsCollection settings = ConfigurationManager.ConnectionStrings;
+            if(settings != null)
+            {
+                foreach(ConnectionStringSettings cs in settings)
+                {
+                    MessageBox.Show("Name = " + cs.Name);
+                    MessageBox.Show("providerName = " + cs.ProviderName);
+                    MessageBox.Show("connectionString = " +cs.ConnectionString);
+                }
+            }
+        }
+
+        private void clickBtnProductQuatity(object sender, EventArgs e)
+        {
+            if (connection.State == ConnectionState.Open)
+            {
+                OleDbCommand oleDbСmd = new OleDbCommand();
+                oleDbСmd.Connection = connection;
+                oleDbСmd.CommandText = "SELECT COUNT (*) FROM Products";
+                int resultQuery = (int)oleDbСmd.ExecuteScalar();
+                lblProductQuantity.Text = resultQuery.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Open connection.");
+            }
+        }
+
+        private void clickBtnProductList(object sender, EventArgs e)
+        {
+            if(connection.State == ConnectionState.Open)
+            {
+                OleDbCommand oleDbCmd = connection.CreateCommand();
+                oleDbCmd.CommandText = "SELECT ProductName FROM Products";
+                OleDbDataReader reader = oleDbCmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    lstProductList.Items.Add(reader["ProductName"].ToString());
+                }
+            }
+            else
+                MessageBox.Show("Open connection.");
+        }
+    }
+}
