b47e9110f5c8871ee20e095560722ebc08c82ad5
Ilia <96974402+Ilia-Ivanov-2022@users.noreply.github.com>
2022-09-16 15:41:00 +0300

    Add files via upload

---
+++ ADO.NET/ITMO.ADO.NET.Lab4.Ex2.CreatingDataTable/FormMain.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace ITMO.ADO.NET.Lab4.Ex2.CreatingDataTable
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AddRowButton = new System.Windows.Forms.Button();
+            this.TableGrid = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.TableGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // AddRowButton
+            // 
+            this.AddRowButton.Location = new System.Drawing.Point(13, 13);
+            this.AddRowButton.Name = "AddRowButton";
+            this.AddRowButton.Size = new System.Drawing.Size(240, 23);
+            this.AddRowButton.TabIndex = 0;
+            this.AddRowButton.Text = "Add Row";
+            this.AddRowButton.UseVisualStyleBackColor = true;
+            this.AddRowButton.Click += new System.EventHandler(this.AddRowButton_Click);
+            // 
+            // TableGrid
+            // 
+            this.TableGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.TableGrid.Location = new System.Drawing.Point(13, 53);
+            this.TableGrid.Name = "TableGrid";
+            this.TableGrid.RowHeadersWidth = 51;
+            this.TableGrid.RowTemplate.Height = 24;
+            this.TableGrid.Size = new System.Drawing.Size(1384, 349);
+            this.TableGrid.TabIndex = 1;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1433, 450);
+            this.Controls.Add(this.TableGrid);
+            this.Controls.Add(this.AddRowButton);
+            this.Name = "FormMain";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.TableGrid)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button AddRowButton;
+        private System.Windows.Forms.DataGridView TableGrid;
+    }
+}
+
---
+++ ADO.NET/ITMO.ADO.NET.Lab4.Ex2.CreatingDataTable/FormMain.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ITMO.ADO.NET.Lab4.Ex2.CreatingDataTable
+{
+    public partial class FormMain : Form
+    {
+        // Table instance.
+        private DataTable CustomersTable = new DataTable("Customers");
+        public FormMain()
+        {
+            InitializeComponent();
+
+            try
+            {
+                // Option of creating table via Form Initialization.
+                // Set DataGridView source as CustomerTable.
+                TableGrid.DataSource = CustomersTable;
+                // Set table schema.
+                CustomersTable.Columns.Add("CustomerID", Type.GetType("System.String"));
+                CustomersTable.Columns.Add("CompanyName", Type.GetType("System.String"));
+                CustomersTable.Columns.Add("ContactName", Type.GetType("System.String"));
+                CustomersTable.Columns.Add("ContactTitle", Type.GetType("System.String"));
+                CustomersTable.Columns.Add("Address", Type.GetType("System.String"));
+                CustomersTable.Columns.Add("City", Type.GetType("System.String"));
+                CustomersTable.Columns.Add("Country", Type.GetType("System.String"));
+                CustomersTable.Columns.Add("Phone", Type.GetType("System.String"));
+                // Set CustomerID row as a Primary Key.
+                DataColumn[] KeyColumns = new DataColumn[1];
+                KeyColumns[0] = CustomersTable.Columns["CustomerID"];
+                CustomersTable.PrimaryKey = KeyColumns;
+                // Set Columns CustomerID and CustomerName to NOT NULL.
+                CustomersTable.Columns["CustomerID"].AllowDBNull = false;
+                CustomersTable.Columns["CustomerName"].AllowDBNull = false;
+            }
+            catch(NullReferenceException exNRE)
+            {
+                MessageBox.Show(exNRE.Message);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+    private void AddRowButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DataRow CustRow = CustomersTable.NewRow();
+                Object[] CustRecord = {"ALFKI", "Alfreds Futterkiste", "Maria Anders",
+                "Sales Representative", "Obere Str. 57", "Berlin",
+                 "Germany", "030-0074321"};
+                CustRow.ItemArray = CustRecord;
+                // Add record into table.
+                CustomersTable.Rows.Add(CustRow);
+            }
+            catch(ConstraintException exConstr)
+            {
+                MessageBox.Show(exConstr.Message);
+            }
+            catch(NullReferenceException exNull)
+            {
+                MessageBox.Show(exNull.Message);
+            }
+            catch(ArgumentException exArg)
+            {
+                MessageBox.Show(exArg.Message);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                MessageBox.Show(ex.GetType().Name);
+            }
+        }
+    }
+}
