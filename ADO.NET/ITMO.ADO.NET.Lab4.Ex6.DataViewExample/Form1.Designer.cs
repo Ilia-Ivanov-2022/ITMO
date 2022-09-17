a76e09d4dc7a0d2c29680c342c3908ce7142be72
Ilia <96974402+Ilia-Ivanov-2022@users.noreply.github.com>
2022-09-17 11:20:29 +0300

    Add files via upload

---
+++ ADO.NET/ITMO.ADO.NET.Lab4.Ex6.DataViewExample/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace ITMO.ADO.NET.Lab4.Ex6.DataViewExample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CustomersGrid = new System.Windows.Forms.DataGridView();
+            this.SortTextBox = new System.Windows.Forms.TextBox();
+            this.FilterTextBox = new System.Windows.Forms.TextBox();
+            this.SetDataViewPropertiesButton = new System.Windows.Forms.Button();
+            this.AddRowButton = new System.Windows.Forms.Button();
+            this.northwindDataSet1 = new ITMO.ADO.NET.Lab4.Ex6.DataViewExample.NorthwindDataSet();
+            this.customersTableAdapter1 = new ITMO.ADO.NET.Lab4.Ex6.DataViewExample.NorthwindDataSetTableAdapters.CustomersTableAdapter();
+            this.ordersTableAdapter1 = new ITMO.ADO.NET.Lab4.Ex6.DataViewExample.NorthwindDataSetTableAdapters.OrdersTableAdapter();
+            this.GetOrdersButton = new System.Windows.Forms.Button();
+            this.OrdersGrid = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.CustomersGrid)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.northwindDataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OrdersGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // CustomersGrid
+            // 
+            this.CustomersGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.CustomersGrid.Location = new System.Drawing.Point(1, -1);
+            this.CustomersGrid.Name = "CustomersGrid";
+            this.CustomersGrid.RowHeadersWidth = 51;
+            this.CustomersGrid.RowTemplate.Height = 24;
+            this.CustomersGrid.Size = new System.Drawing.Size(653, 349);
+            this.CustomersGrid.TabIndex = 0;
+            // 
+            // SortTextBox
+            // 
+            this.SortTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.SortTextBox.Location = new System.Drawing.Point(24, 371);
+            this.SortTextBox.Name = "SortTextBox";
+            this.SortTextBox.Size = new System.Drawing.Size(189, 27);
+            this.SortTextBox.TabIndex = 1;
+            this.SortTextBox.Text = "CustomerID";
+            // 
+            // FilterTextBox
+            // 
+            this.FilterTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FilterTextBox.Location = new System.Drawing.Point(24, 409);
+            this.FilterTextBox.Name = "FilterTextBox";
+            this.FilterTextBox.Size = new System.Drawing.Size(189, 27);
+            this.FilterTextBox.TabIndex = 2;
+            this.FilterTextBox.Text = "City = \'London\'";
+            // 
+            // SetDataViewPropertiesButton
+            // 
+            this.SetDataViewPropertiesButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.SetDataViewPropertiesButton.Location = new System.Drawing.Point(256, 368);
+            this.SetDataViewPropertiesButton.Name = "SetDataViewPropertiesButton";
+            this.SetDataViewPropertiesButton.Size = new System.Drawing.Size(222, 32);
+            this.SetDataViewPropertiesButton.TabIndex = 3;
+            this.SetDataViewPropertiesButton.Text = "SetDataViewProperties";
+            this.SetDataViewPropertiesButton.UseVisualStyleBackColor = true;
+            this.SetDataViewPropertiesButton.Click += new System.EventHandler(this.SetDataViewPropertiesButton_Click);
+            // 
+            // AddRowButton
+            // 
+            this.AddRowButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.AddRowButton.Location = new System.Drawing.Point(504, 368);
+            this.AddRowButton.Name = "AddRowButton";
+            this.AddRowButton.Size = new System.Drawing.Size(222, 32);
+            this.AddRowButton.TabIndex = 4;
+            this.AddRowButton.Text = "Add Row";
+            this.AddRowButton.UseVisualStyleBackColor = true;
+            this.AddRowButton.Click += new System.EventHandler(this.AddRowButton_Click);
+            // 
+            // northwindDataSet1
+            // 
+            this.northwindDataSet1.DataSetName = "NorthwindDataSet";
+            this.northwindDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // customersTableAdapter1
+            // 
+            this.customersTableAdapter1.ClearBeforeFill = true;
+            // 
+            // ordersTableAdapter1
+            // 
+            this.ordersTableAdapter1.ClearBeforeFill = true;
+            // 
+            // GetOrdersButton
+            // 
+            this.GetOrdersButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.GetOrdersButton.Location = new System.Drawing.Point(504, 409);
+            this.GetOrdersButton.Name = "GetOrdersButton";
+            this.GetOrdersButton.Size = new System.Drawing.Size(222, 32);
+            this.GetOrdersButton.TabIndex = 5;
+            this.GetOrdersButton.Text = "Get Orders";
+            this.GetOrdersButton.UseVisualStyleBackColor = true;
+            this.GetOrdersButton.Click += new System.EventHandler(this.GetOrdersButton_Click);
+            // 
+            // OrdersGrid
+            // 
+            this.OrdersGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.OrdersGrid.Location = new System.Drawing.Point(660, -1);
+            this.OrdersGrid.Name = "OrdersGrid";
+            this.OrdersGrid.RowHeadersWidth = 51;
+            this.OrdersGrid.RowTemplate.Height = 24;
+            this.OrdersGrid.Size = new System.Drawing.Size(646, 349);
+            this.OrdersGrid.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1308, 450);
+            this.Controls.Add(this.OrdersGrid);
+            this.Controls.Add(this.GetOrdersButton);
+            this.Controls.Add(this.AddRowButton);
+            this.Controls.Add(this.SetDataViewPropertiesButton);
+            this.Controls.Add(this.FilterTextBox);
+            this.Controls.Add(this.SortTextBox);
+            this.Controls.Add(this.CustomersGrid);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.CustomersGrid)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.northwindDataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OrdersGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView CustomersGrid;
+        private System.Windows.Forms.TextBox SortTextBox;
+        private System.Windows.Forms.TextBox FilterTextBox;
+        private System.Windows.Forms.Button SetDataViewPropertiesButton;
+        private System.Windows.Forms.Button AddRowButton;
+        private NorthwindDataSet northwindDataSet1;
+        private NorthwindDataSetTableAdapters.CustomersTableAdapter customersTableAdapter1;
+        private NorthwindDataSetTableAdapters.OrdersTableAdapter ordersTableAdapter1;
+        private System.Windows.Forms.Button GetOrdersButton;
+        private System.Windows.Forms.DataGridView OrdersGrid;
+    }
+}
+
---
+++ ADO.NET/ITMO.ADO.NET.Lab4.Ex6.DataViewExample/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace ITMO.ADO.NET.Lab4.Ex6.DataViewExample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // Instances of DataView for Customers and Orders.
+        DataView customersDataView;
+        DataView ordersDataView;
+        // Form1 Load event handler.
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Load data in the tables.
+            customersTableAdapter1.Fill(northwindDataSet1.Customers);
+            ordersTableAdapter1.Fill(northwindDataSet1.Orders);
+            // Setup DataSet to use Customers and Orders.
+            customersDataView = new DataView(northwindDataSet1.Customers);
+            ordersDataView = new DataView(northwindDataSet1.Orders);
+            // Setup sorting order.
+            customersDataView.Sort = "CustomerID";
+            // Setup CustomerGrid to display CustomerDataView.
+            CustomersGrid.DataSource = customersDataView;
+        }
+
+        private void SetDataViewPropertiesButton_Click(object sender, EventArgs e)
+        {
+            customersDataView.Sort = SortTextBox.Text;
+            customersDataView.RowFilter = FilterTextBox.Text;
+        }
+
+        private void AddRowButton_Click(object sender, EventArgs e)
+        {
+            // New row.
+            DataRowView newCustomRow = customersDataView.AddNew();
+            // Set values for columns CustomerID and CompanyName.
+            newCustomRow["CustomerID"] = "WINGT";
+            newCustomRow["CompanyName"] = "Wing Tip Toys";
+            // Explicit end of edit.
+            newCustomRow.EndEdit();
+        }
+
+        private void GetOrdersButton_Click(object sender, EventArgs e)
+        {
+            // Get CustomerID for a row selected in CustomersGrid.
+            string selectedCustomerID =
+                (string)CustomersGrid.SelectedCells[0].OwningRow.Cells["CustomerID"].Value;
+            // Create an instance of DataRowView and assign it a selected row.
+            DataRowView selectedRow =
+                customersDataView[customersDataView.Find(selectedCustomerID)];
+            // Call CreateChildView() to move around the reacords and to create a new
+            // DataView based on linked records.
+            ordersDataView =
+                selectedRow.CreateChildView(northwindDataSet1.Relations["FK_Orders_Customers"]);
+            // Set OrderGrid to display linked DataView.
+            OrdersGrid.DataSource = ordersDataView;
+        }
+    }
+}
