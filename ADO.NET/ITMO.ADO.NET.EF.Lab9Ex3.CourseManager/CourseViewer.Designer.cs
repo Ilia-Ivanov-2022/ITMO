23c34d4ae41f7f69446b8d15ef34364fa1edc21f
Ilia <96974402+Ilia-Ivanov-2022@users.noreply.github.com>
2022-09-28 01:47:35 +0300

    Add files via upload

---
+++ ADO.NET/ITMO.ADO.NET.EF.Lab9Ex3.CourseManager/CourseViewer.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace ITMO.ADO.NET.EF.Lab9Ex3.CourseManager
+{
+    partial class CourseViewer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.departmentList = new System.Windows.Forms.ComboBox();
+            this.closeForm = new System.Windows.Forms.Button();
+            this.courseGridView = new System.Windows.Forms.DataGridView();
+            this.saveChanges = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.courseGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // departmentList
+            // 
+            this.departmentList.FormattingEnabled = true;
+            this.departmentList.Location = new System.Drawing.Point(16, 16);
+            this.departmentList.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.departmentList.Name = "departmentList";
+            this.departmentList.Size = new System.Drawing.Size(150, 28);
+            this.departmentList.TabIndex = 0;
+            // 
+            // closeForm
+            // 
+            this.closeForm.Location = new System.Drawing.Point(199, 13);
+            this.closeForm.Name = "closeForm";
+            this.closeForm.Size = new System.Drawing.Size(81, 33);
+            this.closeForm.TabIndex = 1;
+            this.closeForm.Text = "Close";
+            this.closeForm.UseVisualStyleBackColor = true;
+            this.closeForm.Click += new System.EventHandler(this.closeForm_Click);
+            // 
+            // courseGridView
+            // 
+            this.courseGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.courseGridView.Location = new System.Drawing.Point(1, 267);
+            this.courseGridView.Name = "courseGridView";
+            this.courseGridView.RowHeadersWidth = 51;
+            this.courseGridView.RowTemplate.Height = 24;
+            this.courseGridView.Size = new System.Drawing.Size(995, 283);
+            this.courseGridView.TabIndex = 2;
+            // 
+            // saveChanges
+            // 
+            this.saveChanges.Location = new System.Drawing.Point(199, 52);
+            this.saveChanges.Name = "saveChanges";
+            this.saveChanges.Size = new System.Drawing.Size(81, 30);
+            this.saveChanges.TabIndex = 3;
+            this.saveChanges.Text = "Refresh";
+            this.saveChanges.UseVisualStyleBackColor = true;
+            this.saveChanges.Click += new System.EventHandler(this.saveChanges_Click);
+            // 
+            // CourseViewer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1000, 562);
+            this.Controls.Add(this.saveChanges);
+            this.Controls.Add(this.courseGridView);
+            this.Controls.Add(this.closeForm);
+            this.Controls.Add(this.departmentList);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "CourseViewer";
+            this.Text = "Course Viewer";
+            this.Load += new System.EventHandler(this.CourseViewer_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.courseGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox departmentList;
+        private System.Windows.Forms.Button closeForm;
+        private System.Windows.Forms.DataGridView courseGridView;
+        private System.Windows.Forms.Button saveChanges;
+    }
+}
+
---
+++ ADO.NET/ITMO.ADO.NET.EF.Lab9Ex3.CourseManager/CourseViewer.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+using System.Data.Entity.Core.Objects;
+
+
+namespace ITMO.ADO.NET.EF.Lab9Ex3.CourseManager
+{
+    public partial class CourseViewer : Form
+    {
+        // Instance of ObjectContext.
+        private SchoolEntities schoolContext;
+        public CourseViewer()
+        {
+            InitializeComponent();
+        }
+
+        private void CourseViewer_Load(object sender, EventArgs e)
+        {
+            schoolContext = new SchoolEntities();
+            // Query Departments ordered by Name.
+
+            //var departmentQuery = schoolContext.Departments.Include("Courses")
+            //                        .ToList();
+
+            var departmentQuery = from d in schoolContext.Departments.Include("Courses")
+                                  orderby d.Name
+                                  select d;
+            // Display Names of Departments. Connect departmentList to DB.
+            try
+            {
+                this.departmentList.DisplayMember = "Name";
+                this.departmentList.DataSource = departmentQuery.ToList();
+                    //((ObjectQuery)departmentQuery).Execute(MergeOption.AppendOnly);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private DataGridView GetCourseGridView()
+        {
+            return courseGridView;
+        }
+
+        // Display Courses for a selected Department.
+        private void departmentList_SelectedIndexChanged(object sender, EventArgs e, DataGridView courseGridView)
+        {
+            try
+            {
+                Department department = (Department)this.departmentList.SelectedItem;
+
+                courseGridView.DataSource = department.Courses;
+                courseGridView.Columns["Department"].Visible = false;
+                courseGridView.Columns["StudentGrades"].Visible = false;
+                courseGridView.Columns["OnlineCourse"].Visible = false;
+                courseGridView.Columns["OnsiteCourse"].Visible = false;
+                courseGridView.Columns["People"].Visible = false;
+                courseGridView.Columns["DepartmentId"].Visible = false;
+
+                courseGridView.AllowUserToDeleteRows = false;
+                courseGridView.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.AllCells);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void saveChanges_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                schoolContext.SaveChanges();
+                MessageBox.Show("Changes saved to the database.");
+                this.Refresh();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void closeForm_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            schoolContext.Dispose();
+        }
+    }
+}
