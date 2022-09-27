0b962e43368ce0f897823493d5d2365cc1f1d484
Ilia <96974402+Ilia-Ivanov-2022@users.noreply.github.com>
2022-09-28 02:00:47 +0300

    Add files via upload

---
+++ ADO.NET/ITMO.ADO.NET.EF.Lab9.Ex1-2.CodeFirst/Model.cs
@@ -0,0 +1,48 @@
+﻿using System.Data.Entity;
+
+namespace ITMO.ADO.NET.EF.Lab9.Ex1_2.CodeFirst
+{
+    public class Model
+    {
+        public class Customer
+        {
+            public int CustomerId { get; set; }
+            public string Name { get; set; }
+            public string Email { get; set; }
+            public int Age { get; set; }
+            public byte[] Photo { get; set; }
+
+            public override string ToString()
+            {
+                string s = Name + ", электронный адрес: " + Email;
+                return s;
+            }
+            // Ссылка на заказы
+            public virtual List<Order> Orders { get; set; }
+            public Customer()
+            {
+                Orders = new List<Order>();
+            }
+        }
+
+        public class Order
+        {
+            public int OrderId { get; set; }
+            public string ProductName { get; set; }
+            public string Description { get; set; }
+            public int Quantity { get; set; }
+            public DateTime PurchaseDate { get; set; }
+
+            // Ссылка на покупателя
+            public Customer Customer { get; set; }
+
+            public override string ToString()
+            {
+                string s = ProductName + " " + Quantity + "шт., дата: " + PurchaseDate;
+                return s;
+            }
+
+        }
+
+    }
+}
---
+++ ADO.NET/ITMO.ADO.NET.EF.Lab9.Ex1-2.CodeFirst/SampleContext .cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity;
+using static ITMO.ADO.NET.EF.Lab9.Ex1_2.CodeFirst.Model;
+
+namespace ITMO.ADO.NET.EF.Lab9.Ex1_2.CodeFirst
+{
+    public class SampleContext : DbContext
+    {
+        // Ctor of base class with explicit name of DB.
+        public SampleContext() : base("MyShop")
+        { }
+        // References to tables as props.
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+    }
+}
