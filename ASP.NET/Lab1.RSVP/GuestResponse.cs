ab1cfa77129c75707e94f55879c03a6267252af6
Ilia <96974402+Ilia-Ivanov-2022@users.noreply.github.com>
2022-09-29 19:15:53 +0300

    Add files via upload

---
+++ ASP.NET/Lab1.RSVP/GuestResponse.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ITMO.ASP.NET.Lab1.RSVP
+{
+    public class GuestResponse
+    {
+        // Props
+        public int GuestResponseIdId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public bool? WillAttend { get; set; }
+        public DateTime Rdata { get; set; }
+
+        // Ctors
+        public GuestResponse() { }
+        public GuestResponse(string name, string email, string phone, bool? willattend)
+        { 
+            Name = name;
+            Email = email;
+            Phone = phone;
+            WillAttend = willattend;
+            Rdata = DateTime.Now;
+        }
+    }
+}
---
+++ ASP.NET/Lab1.RSVP/ResponseRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ITMO.ASP.NET.Lab1.RSVP
+{
+    public class ResponseRepository
+    {
+        private static ResponseRepository repository = new ResponseRepository();
+        private List<GuestResponse> responses = new List<GuestResponse>();
+        public static ResponseRepository GetRepository()
+        {
+            return repository;
+        }
+        public IEnumerable<GuestResponse> GetAllResponses()
+        {
+            return responses;
+        }
+        public void AddResponse(GuestResponse response)
+        {
+            responses.Add(response);
+        }
+    }
+}
