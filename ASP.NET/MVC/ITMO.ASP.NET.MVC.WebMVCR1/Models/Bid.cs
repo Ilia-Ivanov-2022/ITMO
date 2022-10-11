2cb9b0d49b24e149f9059d108efcf30e24c040a7
Ilia <96974402+Ilia-Ivanov-2022@users.noreply.github.com>
2022-10-11 22:11:39 +0300

    Add files via upload

---
+++ ASP.NET/MVC/ITMO.ASP.NET.MVC.WebMVCR1/Models/Bid.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ITMO.ASP.NET.MVC.Lab4.MvcCreditApp1.Models
+{
+    public class Bid
+    {
+        // ID заявки.
+        public virtual int BidId { get; set; }
+        // Имя заявителя.
+        public virtual string Name { get; set; }
+        // Название кредита.
+        public virtual string CreditHead { get; set; }
+        // Дата подачи заявки.
+        public virtual DateTime bidDate { get; set; }
+    }
+}
