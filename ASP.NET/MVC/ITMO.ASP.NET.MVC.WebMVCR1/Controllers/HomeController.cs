bb890e025b5370fd9af174a8a3ab9e546edbf1b1
Ilia <96974402+Ilia-Ivanov-2022@users.noreply.github.com>
2022-10-11 22:10:07 +0300

    Add files via upload

---
+++ ASP.NET/MVC/ITMO.ASP.NET.MVC.WebMVCR1/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ITMO.ASP.NET.MVC.Lab4.MvcCreditApp1.Models;
+
+namespace ITMO.ASP.NET.MVC.Lab4.MvcCreditApp1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private CreditContext db = new CreditContext();
+        public ActionResult Index()
+        {
+            GiveCredits();
+
+            return View();
+        }
+        private void GiveCredits()
+        {
+            var allCredits = db.Credits.ToList<Credit>();
+            ViewBag.Credits = allCredits;
+        }
+        [HttpGet]
+        public ActionResult CreateBid()
+        {
+            GiveCredits();
+            var allBids = db.Bids.ToList<Bid>();
+            ViewBag.Bids = allBids;
+            return View();
+        }
+        [HttpPost]
+        public string CreateBid(Bid newBid)
+        {
+            newBid.bidDate = DateTime.Now;
+            // Добавляем новую заявку в БД.
+            db.Bids.Add(newBid);
+            // Сохраняем в БД все изменения.
+            db.SaveChanges();
+
+            return "Спасибо, " + newBid.Name + ", за выбор нашего банка." +
+                " Ваша заявка будет рассмотрена в течении 10 дней.";
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
