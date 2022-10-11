906a650956510b55e5bd1cd86dae31169ec16017
Ilia <96974402+Ilia-Ivanov-2022@users.noreply.github.com>
2022-10-11 21:55:18 +0300

    Add files via upload

---
+++ ASP.NET/MVC/ITMO.ASP.NET.MVC.WebMVCR1/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace ITMO.ASP.NET.MVC.Lab4.MvcCreditApp1
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
