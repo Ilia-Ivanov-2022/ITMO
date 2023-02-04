ab55914f5d6527a976d9036950382743e4b12d41
Ilia <96974402+Ilia-Ivanov-2022@users.noreply.github.com>
2023-02-04 14:48:21 +0300

    Add files via upload

---
+++ Cpp/Lab2/Lab2Exercise2_1.cpp
@@ -0,0 +1,25 @@
+﻿// ITMO.CppCourse2023.Lab2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main(){
+
+    system("chcp 1251");
+
+    int a, b, temp;
+
+    std::cout << "a = " << std::endl; 
+    std::cin >> a;
+    std::cout << "b = " << std::endl; 
+    std::cin >> b;
+
+    while (a!=b){
+        if (a > b)
+        a -= b; // аналогично выражению a = a - b
+        else
+        b -= a;
+    }
+    std::cout << "НОД = " << a << std::endl;
+
+}
